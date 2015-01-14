20d582138720b90fce46b15a8af050e7c5622cd5
Sarah GP <sarah.groff.palermo@gmail.com>
2015-01-14 12:02:05 -0500

    Set pins separately, dynamic number of pins

--- neopixel.js
+++ neopixel.js
@@ -2,25 +2,49 @@ var tessel = require('tessel'),
     Npx = require('npx'),
     npx = new Npx(2);
 
-// Copper timer code
-var readPin = tessel.port['GPIO'].digital[2], // G3
-    startTime = +new Date(),
-    timer;
+var patterns = [[], [], [], [], []]; // one spot for each animation
 
-readPin.output(0);
-readPin.on('rise', function(){
-  startTime = +new Date();
-});
 
-function checkTime(now){
-  var pin = readPin.read();
-  console.log(pin);
-  if (pin){
-    return now - startTime;
-  } else {
-    return 0;
+var pins = (function generatePins(){
+  
+  // Pin constuctor
+  function Pin(port){
+    this.pin = tessel.port['GPIO'].digital[port];
+    this.startTime = +new Date();
+    this.timer = 0;
+
+    this.setLow = function(){
+      this.pin.output[0];
+    }
+
+    this.checkTime = function(now){
+      var pin = this.pin.read();
+      if (pin){
+        this.timer = now - this.startTime;
+      } else {
+        this.timer = 0;
+      }
+    }
   }
-}
+
+  // Create array
+  var pinOne = new Pin(2), // G3
+      pinTwo = new Pin(1), // G2
+      arr = [pinOne, pinTwo]; 
+
+  // Set on-rise
+  (function setPins(arr){
+    arr.forEach(function(el){
+      el['pin'].on('rise', function(){
+        this.startTime = +new Date();
+      });
+    });
+  })(arr);
+
+  return arr;
+
+})();
+
 
 // Neopixel code
 
@@ -31,7 +55,6 @@ var animation = npx.newAnimation(5), // initialized with number of animation fra
     animationFour = npx.newAnimation(1),
     flicker = npx.newAnimation(1).setAll('#000000');
 
-// var colorArr = [['#00A775', '#ff4d58'], ['#ff4d58', '#00A775'], ['#a100b3', '#000000'], ['#000000', '#a100b3']];
 
 var colorDict = {
   alive: '#00A775', // eventually reasons[reason] > color
@@ -39,42 +62,31 @@ var colorDict = {
   dead: '#000000'
 };
 
-var patterns = [];
 
-// All alive example
 
 function allAlive(){
   for (var i = 0; i < 5; i++){
     patterns[i] = [colorDict['alive'], colorDict['alive']];
   }
-  console.log(patterns);
 }
-
-
-// #1 Waning
 
 function waning(waningPixel){
   for (var i = 0; i < 5; i++){
     (i % 2) ? 
-      (patterns[i] = [colorDict['wavering'], colorDict['alive']]) : 
-      (patterns[i] = [colorDict['alive'], colorDict['alive']]);
+      (patterns[i][waningPixel] = colorDict['wavering']) : 
+      (patterns[i][waningPixel] = colorDict['alive']);
   }
-  console.log(patterns);
 }
-
-
-// #1 Dead
 
 function dead(deadPixel){
   for (var i = 0; i < 5; i++){
     (i % 4) ? 
-      (patterns[i] = [colorDict['dead'], colorDict['alive']]) : 
-      (patterns[i] = [colorDict['wavering'], colorDict['alive']]);
+      (patterns[i][deadPixel] = colorDict['dead']) : 
+      (patterns[i][deadPixel] = colorDict['wavering']);
     }
-    console.log(patterns);
 }  
 
-function generatePattern(timer){
+function generatePattern(timer, pixel){
 
   switch(true){
     case (timer < 10000):
@@ -82,33 +94,32 @@ function generatePattern(timer){
       break;
 
     case (timer < 60000):
-      waning();
+      waning(pixel);
       break;
 
     case (timer >= 60000):
-      dead();
+      dead(pixel);
       break;
 
     default: 
       console.log('Timer error, no match found');
   }
-
-  console.log(patterns[0])
-
-  animationOne.setPattern(patterns[0]);
-  animationTwo.setPattern(patterns[1]);
-  animationThree.setPattern(patterns[2]);
-  animationFour.setPattern(patterns[3]);
 }
 
 
 function animatePixels(){
 
   var now = +new Date();
-  timer = checkTime(now); // if connection is maintained, timer continues to count
-  console.log('now: ', now, 'timer: ', timer);
 
-  generatePattern(timer);
+  pins.forEach(function(el, index){
+    el.checkTime(now);
+    generatePattern(el.timer, index);
+  });
+
+  animationOne.setPattern(patterns[0]);
+  animationTwo.setPattern(patterns[1]);
+  animationThree.setPattern(patterns[2]);
+  animationFour.setPattern(patterns[3]);
 
   npx.enqueue(animationOne,1000)
      .enqueue(flicker, 50)
@@ -119,6 +130,7 @@ function animatePixels(){
      .enqueue(animationFour,1000)
      .run();
 };
+
 
 setImmediate(animatePixels);
 
