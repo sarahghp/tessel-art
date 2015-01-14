9277608f45eefd9a18797c6104852220f8c43cb4
Sarah GP <sarah.groff.palermo@gmail.com>
2015-01-13 19:11:56 -0500

    Timer driven animation change

--- neopixel.js
+++ neopixel.js
@@ -2,23 +2,54 @@ var tessel = require('tessel'),
     Npx = require('npx'),
     npx = new Npx(2);
 
-var animation = npx.newAnimation(5) // initialized with number of animation frames
+// Copper timer code
+var readPin = tessel.port['GPIO'].digital[0].read(), // G1
+    startTime = +new Date(),
+    timer;
 
-var animationOne = npx.newAnimation(1).setPattern(['#00A775', '#ff4d58']);
-var animationTwo = npx.newAnimation(1).setPattern(['#ff4d58', '#00A775']);
-var animationThree = npx.newAnimation(1).setPattern(['#00A775', '#ff4d58']);
+// setInterval(function(){
+//   console.log(readPin, startTime);
+// }, 1000);
+
+// Neopixel code
+
+var animation = npx.newAnimation(5); // initialized with number of animation frames
+var animationOne = npx.newAnimation(1);
+var animationTwo = npx.newAnimation(1);
+
+var colorArr = [['#00A775', '#ff4d58'], ['#ff4d58', '#00A775'], ['#a100b3', '#072e00'], ['#072e00', '#a100b3']];
+
+function animatePixels(){
+
+  var now = +new Date();
+  timer = now - startTime;
+  console.log('now: ' + now, timer);
+
+  if (timer < 50000){
+    animationOne.setPattern(colorArr[0]);
+    animationTwo.setPattern(colorArr[1]);
+  } else { 
+    animationOne.setPattern(colorArr[2]);
+    animationTwo.setPattern(colorArr[3]);
+  }
+
+  npx.enqueue(animationOne,1000)
+     .enqueue(animationTwo,1000)
+     .enqueue(animationOne,1000)
+     .enqueue(animationTwo,1000)
+     .enqueue(animationOne,1000)
+     .enqueue(animationTwo,1000)
+     .enqueue(animationOne,1000)
+     .enqueue(animationTwo,1000)
+     .enqueue(animationOne,1000)
+     .enqueue(animationTwo,1000)
+     .enqueue(animationOne,1000)
+     .enqueue(animationTwo,1000)
+     .run();
+};
+
+setImmediate(animatePixels);
+setInterval(animatePixels, 10000);
 
 
-npx.enqueue(animationOne,1000)
-   .enqueue(animationTwo,1000)
-   .enqueue(animationOne,1000)
-   .enqueue(animationTwo,1000)
-   .enqueue(animationOne,1000)
-   .enqueue(animationTwo,1000)
-   .enqueue(animationOne,1000)
-   .enqueue(animationTwo,1000)
-   .enqueue(animationOne,1000)
-   .enqueue(animationTwo,1000)
-   .enqueue(animationOne,1000)
-   .enqueue(animationTwo,1000)
-   .run();
+// Pin.on('rise'), start things, then on('fall'), start over
