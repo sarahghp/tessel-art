a19cfd68f51dcea701cc4723ab116b4e0ce0ee54
Sarah GP <sarah.groff.palermo@gmail.com>
2015-01-13 20:34:48 -0500

    Resets timer when circuit opens and closes again

--- neopixel.js
+++ neopixel.js
@@ -3,13 +3,24 @@ var tessel = require('tessel'),
     npx = new Npx(2);
 
 // Copper timer code
-var readPin = tessel.port['GPIO'].digital[0].read(), // G1
+var readPin = tessel.port['GPIO'].digital[2], // G3
     startTime = +new Date(),
     timer;
 
-// setInterval(function(){
-//   console.log(readPin, startTime);
-// }, 1000);
+readPin.output(0);
+readPin.on('rise', function(){
+  startTime = +new Date();
+});
+
+function checkTime(now){
+  var pin = readPin.read();
+  console.log(pin);
+  if (pin){
+    return now - startTime;
+  } else {
+    return 0;
+  }
+}
 
 // Neopixel code
 
@@ -17,13 +28,13 @@ var animation = npx.newAnimation(5); // initialized with number of animation fra
 var animationOne = npx.newAnimation(1);
 var animationTwo = npx.newAnimation(1);
 
-var colorArr = [['#00A775', '#ff4d58'], ['#ff4d58', '#00A775'], ['#a100b3', '#072e00'], ['#072e00', '#a100b3']];
+var colorArr = [['#00A775', '#ff4d58'], ['#ff4d58', '#00A775'], ['#a100b3', '#000000'], ['#000000', '#a100b3']];
 
 function animatePixels(){
 
   var now = +new Date();
-  timer = now - startTime;
-  console.log('now: ' + now, timer);
+  timer = checkTime(now); // if connection is maintained, timer continues to count
+  console.log('now: ', now, 'timer: ', timer);
 
   if (timer < 50000){
     animationOne.setPattern(colorArr[0]);
@@ -37,19 +48,8 @@ function animatePixels(){
      .enqueue(animationTwo,1000)
      .enqueue(animationOne,1000)
      .enqueue(animationTwo,1000)
-     .enqueue(animationOne,1000)
-     .enqueue(animationTwo,1000)
-     .enqueue(animationOne,1000)
-     .enqueue(animationTwo,1000)
-     .enqueue(animationOne,1000)
-     .enqueue(animationTwo,1000)
-     .enqueue(animationOne,1000)
-     .enqueue(animationTwo,1000)
      .run();
 };
 
 setImmediate(animatePixels);
-setInterval(animatePixels, 10000);
-
-
-// Pin.on('rise'), start things, then on('fall'), start over
+setInterval(animatePixels, 4000);
