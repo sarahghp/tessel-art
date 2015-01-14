c52e8fa273e676f548c5b2158d04172a3dd5e694
Sarah GP <sarah.groff.palermo@gmail.com>
2015-01-14 10:16:34 -0500

    Unexpected token bug

--- neopixel.js
+++ neopixel.js
@@ -24,11 +24,77 @@ function checkTime(now){
 
 // Neopixel code
 
-var animation = npx.newAnimation(5); // initialized with number of animation frames
-var animationOne = npx.newAnimation(1);
-var animationTwo = npx.newAnimation(1);
+var animation = npx.newAnimation(5), // initialized with number of animation frames
+    animationOne = npx.newAnimation(1),
+    animationTwo = npx.newAnimation(1),
+    animationThree = npx.newAnimation(1),
+    animationFour = npx.newAnimation(1),
+    flicker = npx.newAnimation(1).setAll('#000000');
 
-var colorArr = [['#00A775', '#ff4d58'], ['#ff4d58', '#00A775'], ['#a100b3', '#000000'], ['#000000', '#a100b3']];
+// var colorArr = [['#00A775', '#ff4d58'], ['#ff4d58', '#00A775'], ['#a100b3', '#000000'], ['#000000', '#a100b3']];
+
+var colorDict = {
+  alive: '#00A775', // eventually reasons[reason] > color
+  wavering: '#ff4d58', // eventually programmatically reduce
+  dead: '#000000'
+};
+
+var patterns = [];
+
+// All alive example
+
+function allAlive(){
+  for (var i = 0; i < 5; i++){
+    patterns[i] = [colorDict[alive], colorDict[alive]];
+  }
+}
+
+
+// #1 Waning
+
+function waning(waningPixel){
+  for (var i = 0; i < 5; i++){
+    (i % 2) ? 
+      (patterns[i] = [colorDict[alive], colorDict[alive]]) : 
+      (patterns[i] = [colorDict[wavering], colorDict[alive]]);
+  }
+}
+
+
+// #1 Dead
+
+function dead(deadPixel){
+  for (var i = 0; i < 5; i++){
+    (i % 4) ? 
+      (patterns[i] = [colorDict[alive], colorDict[alive]]) : 
+      (patterns[i] = [colorDict[dead], colorDict[alive]]);
+}  
+
+function generatePattern(timer){
+
+  switch(timer){
+    case (timer < 10000):
+      allAlive();
+      break;
+
+    case (timer < 60000):
+      waning();
+      break;
+
+    case (timer >= 60000):
+      dead();
+      break;
+
+    default: 
+      console.log('Timer error, no match found');
+  }
+
+  animationOne.setPattern([patterns[0]]);
+  animationTwo.setPattern([patterns[1]]);
+  animationThree.setPattern([patterns[2]]);
+  animationFour.setPattern([patterns[3]]);
+}
+
 
 function animatePixels(){
 
@@ -36,20 +102,21 @@ function animatePixels(){
   timer = checkTime(now); // if connection is maintained, timer continues to count
   console.log('now: ', now, 'timer: ', timer);
 
-  if (timer < 50000){
-    animationOne.setPattern(colorArr[0]);
-    animationTwo.setPattern(colorArr[1]);
-  } else { 
-    animationOne.setPattern(colorArr[2]);
-    animationTwo.setPattern(colorArr[3]);
-  }
+  generatePattern(timer);
 
   npx.enqueue(animationOne,1000)
-     .enqueue(animationTwo,1000)
-     .enqueue(animationOne,1000)
-     .enqueue(animationTwo,1000)
+     .enqueue(flicker, 100)
+     .enqueue(animationTwo, 1000)
+     .enqueue(flicker, 100)
+     .enqueue(animationThree,1000)
+     .enqueue(flicker, 100)
+     .enqueue(animationFour,1000)
      .run();
 };
 
-setImmediate(animatePixels);
-setInterval(animatePixels, 4000);
+// setImmediate(animatePixels);
+
+// it's likely still that the interval will be offset with the enqueue interval 
+// but that is desirable in this case
+
+setInterval(animatePixels, 4500);
