f271af5b2631839281accdff4ad8b72e96867d5d
Sarah GP <sarah.groff.palermo@gmail.com>
2015-01-22 23:03:44 -0500

    Stable calibrate for dev for now

--- test-calibrate.js
+++ test-calibrate.js
@@ -1,18 +1,17 @@
 var tessel = require('tessel'),
     ambientlib = require('ambient-attx4'),
     ambient = ambientlib.use(tessel.port['A']),
-    calibrate = require('./util/calibrate');
+    calibrate = require('./util/calibrate/calibrate-stable');
 
-// var soundHigh, soundLow;
+var soundHigh, soundLow;
 
-// function userCall(low, high) {
-//   soundLow = low;
-//   soundHigh = high;
-//   console.log(soundLow, soundHigh);
-// }
+function userCall(low, high) {
+  soundLow = low;
+  soundHigh = high;
+  console.log(soundLow, soundHigh);
+}
 
 ambient.on('ready', function(){
-  var test = calibrate.set(ambient, 'getSoundBuffer');
-  console.log(test);
+  calibrate.set(ambient, 'getSoundBuffer', userCall);
 });
 
---
+++ util/calibrate/calibrate-stable.js
@@ -0,0 +1,23 @@
+function calibrate(mod, meth, userCall){
+  var highest = 0,
+      lowest = 1;
+
+  function callback(err, data){
+    console.log('called');
+    if (err){
+        console.error(err);
+    }
+
+    for (var i = 0, l = data.length; i < l; i++){
+      (data[i] < lowest) && (lowest = data[i]);
+      (data[i] > highest) && (highest = data[i]);
+    }
+    userCall(lowest, highest);
+  }
+
+  mod[meth].call(mod, callback);
+
+}
+
+
+exports.set = calibrate;
