6b20ff89eb289bed26d85c257df84e9d4cae5204
Sarah GP <sarah.groff.palermo@gmail.com>
2015-01-24 18:47:56 -0500

    Behavior refinement, driven by web

--- spiny/spiny.js
+++ spiny/spiny.js
@@ -7,20 +7,29 @@ var pubnub = require("pubnub").init({
   subscribe_key: "sub-c-e9a1c52e-9ab9-11e4-a626-02ee2ddab7fe" 
 });
 
-var target = .5; // Eventually will be driven by pubnub input
+var target = 0,
+    init = 0; // Eventually will be driven by pubnub input
 
 function moveToTarget(target){
-  for (var i = 0, t = target; i < t; i += 0.05){
-    (function(j){
-      setTimeout(function move(){
-        console.log('Position:', j);
-        servo.move(1, j);
-      }, j * 10000);
-    })(i);
+
+  if (target == 0){
+    servo.move(1, 0);
+  } else { 
+    for (var i = init, t = target; i < t; i += 0.05){
+      (function(j){
+        setTimeout(function move(){
+          console.log('Position:', j);
+          servo.move(1, j);
+        }, j * 10000);
+      })(i);
+    }    
   }
+  
+  init = target;
 }
 
 servo.on('ready', function () {
+  console.log('Running');
   servo.configure(1, 0.05, 0.14, function () {
     moveToTarget(target);  
   });
@@ -29,7 +38,7 @@ servo.on('ready', function () {
     channel: "spiny-servo",
     message: function(m){
       console.log('Message received:', m);
-      moveToTarget(+m);
+      moveToTarget(+m * .1);
     }
   });
 
