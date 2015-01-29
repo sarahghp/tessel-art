ffcfd25412b153e015addb20b920e1ec54843960
Sarah GP <sarah.groff.palermo@gmail.com>
2015-01-28 22:45:48 -0500

    I can count down

--- spiny/spiny.js
+++ spiny/spiny.js
@@ -8,31 +8,48 @@ var pubnub = require("pubnub").init({
 });
 
 var target = 0,
-    init = 0; // Eventually will be driven by pubnub input
+    init = 0;
 
 function moveToTarget(target){
 
-  if (target == 0){
-    servo.move(1, 0);
-  } else { 
-    for (var i = init, t = target; i < t; i += 0.05){
+  if (target > init){
+    console.log('Before execution:', init);
+      for (var i = init, t = target; i < t; i += 0.05){
+        (function(j){
+          setTimeout(function moveUp(){
+            console.log('Up Position:', 0.95-j);
+            // servo.move(1, 0.95-j);
+          }, j * 10000);
+        })(i);
+      } 
+    init = target;
+    console.log('After move up', init);
+  } else if (target < init && Math.abs(target) > 0.01) {
+    for (var i = init, t = target; i > t; i -= 0.05){
       (function(j){
-        setTimeout(function move(){
-          console.log('Position:', 1-j);
-          servo.move(1, 1-j);
-        }, j * 10000);
+        setTimeout(function moveDown(){
+          console.log('Down Position:', 0.95-j);
+          // servo.move(1, 0.95-j);
+        }, (0.95-j) * 10000);
       })(i);
-    }    
+    }
+    init = target;
+  } else if (Math.abs(target) < 0.1){
+    for (var i = init; 0 < i; i-= 0.05){
+      (function(j){
+        setTimeout(function moveDown(){
+          console.log('0 Position:', 0.95-j);
+          // servo.move(1, 0.95-j);
+        }, (0.95-j) * 10000);
+      })(i);
+    }
+    init = 0;
   }
-  
-  init = target;
 }
 
 servo.on('ready', function () {
   console.log('Running');
-  servo.configure(1, 0.05, 0.14, function () {
-    moveToTarget(target);  
-  });
+  servo.configure(1, 0.05, 0.10);
   
   pubnub.subscribe({
     channel: "spiny-servo",
