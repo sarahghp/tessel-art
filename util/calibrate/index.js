4768f9b3f701008404653dfb380b45b5dea3529d
Sarah GP <sarah.groff.palermo@gmail.com>
2015-02-27 16:52:56 -0500

    Rewrote for user object to actually work, now back to events

--- util/calibrate/index.js
+++ util/calibrate/index.js
@@ -1,73 +1,103 @@
 //TODO: Add event interface
 
-var RSVP = require('rsvp');
 
-function get(mod, meth, options){
 
-  var notBuffer   = options.returnsSingle || false,            
-      bufferCalls = options.calls || 10,
-      fetchedData = options.thresholds ?
-                    // reverse values for threshold test 
-                    { high: options.thresholds.low, low: options.thresholds.high } : 
-                    // defaults to assuming function returns values between 0 and 1
-                    { high: 0, low: 1 };
 
-  var calibrate = new RSVP.defer(),
-      promise = calibrate.promise;
+var util = require('util'),
+    EventEmitter = require('events').EventEmitter,
+    RSVP = require('rsvp');
 
-  function dataCall(err, data){
-    if (err){
-      console.log(err);
-    } 
-    calibrate.resolve(data);
-  }
 
-  function pushData(num){
-    var promises = [];
 
-    function getValue(){
-      return new RSVP.Promise(function(resolve, reject){
-        mod[meth].call(mod, function(err, data){
-          if (err){
-            reject(err);
-          } else {
-            console.log('resolved', data);
-            resolve(data);
-          }
+
+function Calibrate(){
+
+  var self = this;
+
+  this.get = function (mod, meth, userOptions){
+
+      var options = {
+        returnsSingle: false,
+        calls: 10,
+        thresholds: { high: 1, low: 0 }
+      }
+
+      if (userOptions){
+        userOptions.returnsSingle && (options.returnsSingle = userOptions.returnsSingle);
+        userOptions.calls && (options.calls = userOptions.calls);
+        userOptions.thresholds && (options.thresholds = userOptions.thresholds);
+      }
+
+      var notBuffer   = options.returnsSingle,            
+          bufferCalls = options.call,
+          fetchedData = { high: options.thresholds.low, low: options.thresholds.high }; // reverse values for threshold test 
+                        
+
+      var calibrate = new RSVP.defer(),
+          promise = calibrate.promise;
+
+      function dataCall(err, data){
+        if (err){
+          console.log(err);
+        } 
+        calibrate.resolve(data);
+      }
+
+      function pushData(num){
+        var promises = [];
+
+        function getValue(){
+          return new RSVP.Promise(function(resolve, reject){
+            mod[meth].call(mod, function(err, data){
+              if (err){
+                reject(err);
+              } else {
+                console.log('resolved', data);
+                resolve(data);
+              }
+            });
+          });
+        }
+
+        for (var i = 0; i < num; i++){
+          promises.push(getValue());
+        }
+
+        RSVP.all(promises)
+            .then(function(arr){calibrate.resolve(arr)});
+      }
+
+      function threshold(arr){
+        console.log('threshold called', arr);
+        for (var i = 0, l = arr.length; i < l; i++){
+            (arr[i] < fetchedData.low) && (fetchedData.low = arr[i]);
+            (arr[i] > fetchedData.high) && (fetchedData.high = arr[i]);
+        }
+        self.emit('calibrated', fetchedData); // emit event for those who prefer event interface
+        return fetchedData;     // return data for those using .then()
+      }
+
+      (function getData(){
+        if (!notBuffer) {
+          mod[meth].call(mod, dataCall);   // if the method returns an array, we can just call it once
+        } else {
+          pushData(bufferCalls);          // otherwise pushData will call repeatedly to build a buffer manually
+        }
+      })();
+
+     return promise
+        .then(threshold)
+        .catch(function(err){
+          console.log("Error: ", err);
         });
-      });
     }
-
-    for (var i = 0; i < num; i++){
-      promises.push(getValue());
-    }
-
-    RSVP.all(promises)
-        .then(function(arr){calibrate.resolve(arr)});
-  }
-
-  function threshold(arr){
-    console.log('threshold called', arr);
-    for (var i = 0, l = arr.length; i < l; i++){
-        (arr[i] < fetchedData.low) && (fetchedData.low = arr[i]);
-        (arr[i] > fetchedData.high) && (fetchedData.high = arr[i]);
-    }
-    return fetchedData;
-  }
-
-  (function getData(){
-    if (!notBuffer) {
-      mod[meth].call(mod, dataCall);   // if the method returns an array, we can just call it once
-    } else {
-      pushData(bufferCalls);          // otherwise pushData will call repeatedly to build a buffer manually
-    }
-  })();
-
- return promise
-    .then(threshold)
-    .catch(function(err){
-      console.log("Error: ", err);
-    });
 }
 
-exports.get = get;
+
+util.inherits(Calibrate, EventEmitter);
+
+var calibrate = new Calibrate();
+
+
+
+exports.get = calibrate.get;
